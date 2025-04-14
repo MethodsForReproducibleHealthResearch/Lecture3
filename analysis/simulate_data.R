65ad21c99dbe7e63381aac0cb86f601c100abef5
Sherri Rose <sherrirose@stanford.edu>
2025-04-14 16:37:12 -0700

    code style

--- analysis/simulate_data.R
+++ analysis/simulate_data.R
@@ -5,13 +5,13 @@
 ##Initial Example
 
 set.seed(27)
-n<-1000
+n <- 1000
 sim_data <- data.frame(X=runif(n, min=.5, max=1))
 sim_data <- transform(sim_data, #add Y
                  Y=rnorm(n, 4*X, sd=.5))
 
 summary(sim_data)
-write.csv(sim_data,"data/sim_data.csv", row.names = FALSE)
+write.csv(sim_data,"data/sim_data.csv", row.names=FALSE)
 
 plot(sim_data$X, sim_data$Y, xlab="X", ylab="Y")
 
@@ -19,13 +19,13 @@ plot(sim_data$X, sim_data$Y, xlab="X", ylab="Y")
 ##Change Strength of Relationship
 
 set.seed(27)
-n<-1000
-sim_data2 <- data.frame(X=runif(n, min = .5, max = 1))
+n <- 1000
+sim_data2 <- data.frame(X=runif(n, min=.5, max=1))
 sim_data2 <- transform(sim_data2, #add Y
                       Y=rnorm(n, 2*X, sd=.5))
 
 summary(sim_data2)
-write.csv(sim_data2,"data/sim_data2.csv", row.names = FALSE)
+write.csv(sim_data2,"data/sim_data2.csv", row.names=FALSE)
 
 plot(sim_data2$X, sim_data2$Y, xlab="X", ylab="Y")
 
